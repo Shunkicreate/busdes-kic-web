de750fcf26ca343494e4af69a54d4095ee1e8411
YukaChoco <sugar.ore.yk@gmail.com>
2023-02-11 10:50:42 +0900

    img

--- src/atom/TabbarButton.tsx
+++ src/atom/TabbarButton.tsx
@@ -1,7 +1,10 @@
-const TabbarButton = () => {
+import { mode } from "../types/main.type"
+export const TabbarButton = ({mode,modeName,switchMode}:{mode:mode,modeName:string,switchMode:(prm:mode)=>void}) => {
     return(
-        <div></div>
+        <div>            
+            <button onClick={() => { switchMode(mode) }} className="bg-gray-300 hover:bg-gray-200 text-white rounded px-4 py-2">{modeName}</button>
+        </div>
     )
 }
 
-export default TabbarButton
+// export default TabbarButton
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,11 +1,14 @@
 import { mode } from "../types/main.type"
+import { TabbarButton } from "../atom/TabbarButton"
 export const Footer = ({ setMode }: { setMode: React.Dispatch<React.SetStateAction<mode>> }) => {
     const switchMode = (mode: mode) => {
         setMode(mode)
     }
     return (
         <div>
-            <div>
+            {/* { setMode } */}
+            {/* <div>
+                <button className="bg-gray-300 hover:bg-gray-200 text-white rounded px-4 py-2">Gray 300</button>
                 <button onClick={() => { switchMode("TimeTable") }}>時刻表</button>
             </div>
             <div>
@@ -13,7 +16,10 @@ export const Footer = ({ setMode }: { setMode: React.Dispatch<React.SetStateActi
             </div>
             <div>
                 <button onClick={() => { switchMode("Settings") }}>設定</button>
-            </div>
+            </div> */}
+            <TabbarButton mode="TimeTable" modeName="時刻表" switchMode={switchMode}></TabbarButton>
+            <TabbarButton mode="NextBus" modeName="次のバス" switchMode={switchMode}></TabbarButton>
+            <TabbarButton mode="Settings" modeName="設定" switchMode={switchMode}></TabbarButton>
         </div>
     )
 }
