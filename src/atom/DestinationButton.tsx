224dbcb406713640e78c7b07c1506df9e7a2da86
YukaChoco <sugar.ore.yk@gmail.com>
2023-02-12 15:30:35 +0900

    destinationButton3-imges

--- src/atom/DestinationButton.tsx
+++ src/atom/DestinationButton.tsx
@@ -1,9 +1,10 @@
 import React from "react"
-import { ReactComponent as Icon } from '../ReverseButton.svg'
-//ボタン使うファイルで import { DestinationButton } from "./atom/DestinationButton" する
+import { ReactComponent as Icon } from '../images/ReverseButton.svg'
+// ボタン使うファイルで import { DestinationButton } from "./atom/DestinationButton" する
+// 関数内のdivタグにonClick={ }使う
 export const DestinationButton = () => {
     return (
-        <div> //このdivタグにonClick={ }使う
+        <div>
             <Icon />
         </div>
     )
