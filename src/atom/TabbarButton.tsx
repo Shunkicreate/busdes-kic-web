4d6b7beecb9d58b3aa1394b4291bba98c107b189
YukaChoco <sugar.ore.yk@gmail.com>
2023-02-12 12:34:58 +0900

    tabbarbutton2

--- src/atom/TabbarButton.tsx
+++ src/atom/TabbarButton.tsx
@@ -1,26 +1,10 @@
 import { mode } from "../types/main.type"
-// import React from "react"
-import React, { useState } from 'react';
-// export const TabbarButton = ({mode,modeName,switchMode}:{mode:mode,modeName:string,switchMode:(prm:mode)=>void}) => {
-export const TabbarButton = ({mode,modeName,color,switchMode}:{mode:mode,modeName:string,color:string,switchMode:(prm:mode)=>void}) => {
-
-    // let colorTab = "gray-200";
-    // colorTab = "gray-100";
-    // const changeText = (e:string) => {
-    //     setColorTab(e);
-    // };
-    // changeText('gray-100');
-    // setColorTab('bg-gray-100');
-    // console.log({colorTab});
-
-
-    return(
+import React from "react"
+export const TabbarButton = ({ mode, modeName, color, switchMode }: { mode: mode, modeName: string, color: string, switchMode: (prm: mode) => void }) => {
+    return (
         <div className="px-5 pt-2 text-1xl text-center">
-            {/* <button onClick={() => { switchMode(mode) }} className={'bg-${colorTab} w-20 h-20'}>{modeName}</button>        */}
             <button onClick={() => { switchMode(mode); }} className={`${color} w-12 h-12 rounded-full`}></button>
             <p>{modeName}</p>
         </div>
     )
 }
-
-// export default TabbarButton
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,40 +1,27 @@
 import { mode } from "../types/main.type"
 import { TabbarButton } from "../atom/TabbarButton"
-// import React from "react"
-import React, { useState } from 'react';
+import React from "react"
 
-export const Footer = ({ setMode,currentMode }: { setMode: React.Dispatch<React.SetStateAction<mode>>, currentMode:mode }) => {
+export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React.SetStateAction<mode>>, currentMode: mode }) => {
     const switchMode = (mode: mode) => {
         setMode(mode)
     }
-    const color = (mode:mode,currentMode:mode) => {
-        if(mode===currentMode){
+    const color = (mode: mode, currentMode: mode) => {
+        if (mode === currentMode) {
             return 'bg-yellow-300'
         }
-        else{
+        else {
             return 'bg-gray-100'
         }
     }
 
     return (
-        <div className="">
+        <div>
             <div className="flex justify-center">
-                {/* { setMode } */}
-                {/* <div>
-                    <button className="bg-gray-300 hover:bg-gray-200 text-white rounded px-4 py-2">Gray 300</button>
-                    <button onClick={() => { switchMode("TimeTable") }}>時刻表</button>
-                </div>
-                <div>
-                    <button onClick={() => { switchMode("NextBus") }}>次のバス</button>
-                </div>
-                <div>
-                    <button onClick={() => { switchMode("Settings") }}>設定</button>
-                </div> */}
-                <TabbarButton mode="TimeTable" modeName="時刻表" color={color("TimeTable",currentMode)} switchMode={switchMode}></TabbarButton>
-                <TabbarButton mode="NextBus" modeName="次のバス" color={color("NextBus",currentMode)} switchMode={switchMode}></TabbarButton>
-                <TabbarButton mode="Settings" modeName="設定" color={color("Settings",currentMode)} switchMode={switchMode}></TabbarButton>
+                <TabbarButton mode="TimeTable" modeName="時刻表" color={color("TimeTable", currentMode)} switchMode={switchMode}></TabbarButton>
+                <TabbarButton mode="NextBus" modeName="次のバス" color={color("NextBus", currentMode)} switchMode={switchMode}></TabbarButton>
+                <TabbarButton mode="Settings" modeName="設定" color={color("Settings", currentMode)} switchMode={switchMode}></TabbarButton>
             </div>
-
         </div>
     )
 }
