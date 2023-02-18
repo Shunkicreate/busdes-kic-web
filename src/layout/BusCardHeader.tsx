c45a3318cb8849e9dfad5602309a230316686829
Kyohei <gyoroemon0929@gmail.com>
2023-02-19 00:12:33 +0900

    gridでデザインできた,べた打ち直す

--- src/layout/BusCardHeader.tsx
+++ src/layout/BusCardHeader.tsx
@@ -2,16 +2,20 @@ import React from 'react';
 import BusArrow from "./BusArrow.svg";
 
 
+
+
 const BusCardHeader = () => {
 
-    return (
 
-        <div className="flex justify-center flex-row h-16">
-            <div className="p-6 justify-center text-xl">立命館大学</div>
-            <div className="inline-flex">
+    
+
+    return (
+        <div className="grid grid-cols-3 h-16 place-content-center">
+            <div className="text-center text-xl">立命館大学</div>
+            <div className="flex justify-center">
                 <img className="" src={BusArrow} alt="BusArrow" width="45" />
             </div>
-            <div className="p-6 justify-center text-xl">京都駅</div>
+            <div className="text-center text-xl">京都駅</div>
         </div>
     )
 }
