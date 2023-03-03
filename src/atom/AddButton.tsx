8ff1deb438a4e737cdc402c7660d6410c060e9bf
Kyohei <gyoroemon0929@gmail.com>
2023-03-03 14:56:37 +0900

    がんばります

---
+++ src/atom/AddButton.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+// ボタン使うファイルで import { DestinationButton } from "./atom/DestinationButton" する
+// 関数内のdivタグにonClick={ }使う
+
+export const AddButton = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
