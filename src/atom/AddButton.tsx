0b81ab681d58b2bfb16495b4cfb457d04349f59a
Kyohei <gyoroemon0929@gmail.com>
2023-03-03 15:38:41 +0900

    とりあえずFooterに配置

--- src/atom/AddButton.tsx
+++ src/atom/AddButton.tsx
@@ -1,12 +1,12 @@
 import React from "react"
 
-// ボタン使うファイルで import { DestinationButton } from "./atom/DestinationButton" する
+// ボタン使うファイルで import { AddButton } from "./atom/AddButton" する
 // 関数内のdivタグにonClick={ }使う
 
 export const AddButton = () => {
     return (
-        <div>
-            
+        <div className="text-4xl text-white">
+            <button className="rounded-full bg-yellow-300 w-12 h-12">+</button>
         </div>
     )
 }
--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,6 +2,9 @@ import { mode } from "../types/main.type"
 import { TabbarButton } from "../atom/TabbarButton"
 import React from "react"
 
+
+import { AddButton } from "../atom/AddButton"
+
 export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React.SetStateAction<mode>>, currentMode: mode }) => {
     const switchMode = (mode: mode) => {
         setMode(mode)
@@ -21,6 +24,7 @@ export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React
                 <TabbarButton mode="TimeTable" modeName="時刻表" color={color("TimeTable", currentMode)} switchMode={switchMode}></TabbarButton>
                 <TabbarButton mode="NextBus" modeName="次のバス" color={color("NextBus", currentMode)} switchMode={switchMode}></TabbarButton>
                 <TabbarButton mode="Settings" modeName="設定" color={color("Settings", currentMode)} switchMode={switchMode}></TabbarButton>
+                <AddButton></AddButton>
             </div>
         </div>
     )
