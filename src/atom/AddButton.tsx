9f55dbd904db52b91c5d4547a45c5e0f0d1191af
Kyohei <gyoroemon0929@gmail.com>
2023-03-11 03:57:09 +0900

    svgに変更

--- src/atom/AddButton.tsx
+++ src/atom/AddButton.tsx
@@ -1,12 +1,13 @@
 import React from "react"
+import Addbutton from "../images/addButton.svg"
 
 // ボタン使うファイルで import { AddButton } from "./atom/AddButton" する
 // 関数内のdivタグにonClick={ }使う
 
 export const AddButton = () => {
     return (
-        <div className="text-4xl text-white">
-            <button className="rounded-full bg-yellow-300 w-12 h-12">+</button>
-        </div>
+        <button>
+            <img className="" src={Addbutton} alt="AddButton" width="50"/>
+        </button>
     )
 }
