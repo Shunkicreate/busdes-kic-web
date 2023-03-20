44e0b0bc6c15e169350ac617a4bbdae45a3af1c4
YukaChoco <sugar.ore.yk@gmail.com>
2023-03-21 00:40:47 +0900

    texttyousei

--- src/atom/TabbarButton.tsx
+++ src/atom/TabbarButton.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const TabbarButton = ({ mode, modeName, color, switchMode }: { mode: mode, modeName: string, color: string, switchMode: (prm: mode) => void }) => {
     return (
         // <div className='px-5 pt-2 text-1xl text-center'>
-        <div className='text-1xl text-center'>
-            <button onClick={() => { switchMode(mode); }} className={`${color} w-12 h-12 rounded-full`}></button>
-            <p>{modeName}</p>
+        <div className='mt-3 mb-5 text-1xl text-center'>
+            <button onClick={() => { switchMode(mode); }} className={`${color} w-11 h-11 rounded-full`}></button>
+            <p className='text-xs'>{modeName}</p>
         </div>
     )
 }
