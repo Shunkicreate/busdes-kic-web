335240351d9bf0c88bcaab3738b8895d2c1ac7c7
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-03-22 15:34:12 +0900

    クリックするとその場所に移動するところまで書いた

--- src/layout/TimeTable.tsx
+++ src/layout/TimeTable.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { TimeTable, OneBusTime, unionDays, AllBusStopsType, TimeTableResponse, busStopListAtomType } from '../types/Bus.type';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 import getAllBusStopList from '../grobalState/selectors/getAllBusStopList';
@@ -7,32 +7,7 @@ import { ApiClient } from '../lib/api-client';
 import addAllBusStopListSelector from '../grobalState/selectors/addAllBusStopList';
 import { fetchTimeTable, ShowOneDayBusTime } from '../functions/TimeTableFunctions';
 import Arrow from '../images/Arrow.svg'
-
-const TimeTableHeader = ({ fr, to }: { fr: AllBusStopsType, to: AllBusStopsType }) => {
-    return (
-        <div className='text-center'>
-            <div className='grid grid-cols-3'>
-                <div className=''>
-                    <strong>出発</strong>
-                </div>
-                <div className=''>
-                    <strong>{fr}</strong>
-                </div>
-            </div>
-            <div>
-                <img src={Arrow} alt="Arrow image" className='m-auto my-1' />
-            </div>
-            <div className='grid grid-cols-3'>
-                <div className=''>
-                    <strong>到着</strong>
-                </div>
-                <div className=''>
-                    <strong>{to}</strong>
-                </div>
-            </div>
-        </div>
-    )
-}
+import ReverseButton from '../images/ReverseButton.svg'
 
 
 export const ShowTimeTable = () => {
@@ -40,7 +15,51 @@ export const ShowTimeTable = () => {
     const AllBusStopList = useRecoilValue(getAllBusStopList)
     const [currentFromBusStop, setCurrentFromBusStop] = useState<AllBusStopsType>(AllBusStopList[0].fr)
     const [currenToBusStop, setCurrenToBusStop] = useState<AllBusStopsType>(AllBusStopList[0].to)
+    const timetableRefs = useRef<HTMLDivElement[]>([])
 
+    const TimeTableHeader = ({ fr, to }: { fr: AllBusStopsType, to: AllBusStopsType }) => {
+        // const [isRotated, setIsRotated] = useState(true);
+        // const handleAnimationEnd = () => {
+        //     setIsRotated(true);
+        // };
+        return (
+            <div className='text-center relative'>
+                <div className=''>
+                    <div className='grid grid-cols-3'>
+                        <div className=''>
+                            <strong>出発</strong>
+                        </div>
+                        <div className=''>
+                            <strong>{fr}</strong>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={Arrow} alt="Arrow image" className='m-auto my-1' />
+                    </div>
+                    <div className='grid grid-cols-3'>
+                        <div className=''>
+                            <strong>到着</strong>
+                        </div>
+                        <div className=''>
+                            <strong>{to}</strong>
+                        </div>
+                    </div>
+                </div>
+                {/* <div
+                    className={`absolute top-4 right-4 w-fit h-fit box ${isRotated ? 'rotated' : ''}`}
+                    onAnimationEnd={handleAnimationEnd}
+                >
+                    dsfasfds
+                    <img src={ReverseButton} alt="" />
+                </div> */}
+                <div className='absolute top-4 right-4 w-fit h-fit'>
+                    <img src={ReverseButton} alt="" />
+                </div>
+            </div>
+        )
+    }
+
+    //ここの処理を非同期で上手くリファクタする！！！！！
     useEffect(() => {
         AllBusStopList.forEach((BusStop) => {
             if (BusStop.TimeTableData === undefined) {
@@ -59,14 +78,29 @@ export const ShowTimeTable = () => {
         })
     }, [])
 
+    const scrollToTimatable = (i: number) => {
+        timetableRefs.current[i].scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
+    }
+
     return (
         <div className='mx-4'>
             <TimeTableHeader fr={currentFromBusStop} to={currenToBusStop}></TimeTableHeader>
+            {
+                AllBusStopList.map((BusStop, i) => {
+                    return (
+                        <div key={i} onClick={() => { scrollToTimatable(i) }}>{BusStop.to}</div>
+                    )
+                })
+            }
             <div className="flex bg-white overflow-scroll whitespace-normal">
                 {
                     AllBusStopList.map((BusStop, i) => {
                         return (
-                            <div key={i} className='timetable'>
+                            <div key={i} className='timetable' ref={(el) => {
+                                if (el) {
+                                    timetableRefs.current[i] = el
+                                }
+                            }}>
                                 {BusStop.TimeTableData ? <ShowOneDayBusTime timeTable={BusStop.TimeTableData}></ShowOneDayBusTime> : <></>}
                             </div>
                         )
