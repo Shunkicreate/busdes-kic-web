cff11d10f86656d320b120c91e9c3565fca727f7
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-03-23 00:42:18 +0900

    とりあえずスクロールができるようになった！！！！おやすみ！！！！

--- src/layout/TimeTable.tsx
+++ src/layout/TimeTable.tsx
@@ -8,7 +8,26 @@ import addAllBusStopListSelector from '../grobalState/selectors/addAllBusStopLis
 import { fetchTimeTable, ShowOneDayBusTime } from '../functions/TimeTableFunctions';
 import Arrow from '../images/Arrow.svg'
 import ReverseButton from '../images/ReverseButton.svg'
+import { Swiper, SwiperSlide, SwiperRef } from 'swiper/react';
+import 'swiper/css';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
+const SwiperTab = () => {
+    return (
+        <Swiper
+            spaceBetween={50}
+            slidesPerView={1}
+            onSlideChange={() => console.log('slide change')}
+            onSwiper={(swiper) => console.log(swiper)}
+        >
+            <SwiperSlide>Slide 1</SwiperSlide>
+            <SwiperSlide>Slide 2</SwiperSlide>
+            <SwiperSlide>Slide 3</SwiperSlide>
+        </Swiper>
+    );
+};
 
 export const ShowTimeTable = () => {
     const addAllBusStopList = useSetRecoilState(addAllBusStopListSelector)
@@ -16,6 +35,19 @@ export const ShowTimeTable = () => {
     const [currentFromBusStop, setCurrentFromBusStop] = useState<AllBusStopsType>(AllBusStopList[0].fr)
     const [currenToBusStop, setCurrenToBusStop] = useState<AllBusStopsType>(AllBusStopList[0].to)
     const timetableRefs = useRef<HTMLDivElement[]>([])
+    const [value, setValue] = useState(0);
+    const swiperRef = useRef<SwiperRef>(null);
+    const handleChange = (i: number) => {
+        console.log('handle changed')
+        setValue(i);
+        changeSlide(i)
+    }
+
+    const changeSlide = (i: number) => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideTo(i)
+        }
+    };
 
     const TimeTableHeader = ({ fr, to }: { fr: AllBusStopsType, to: AllBusStopsType }) => {
         // const [isRotated, setIsRotated] = useState(true);
@@ -92,20 +124,41 @@ export const ShowTimeTable = () => {
                     )
                 })
             }
+            <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+                <Tabs value={value} onChange={(e, newTab) => handleChange(newTab)} centered>
+                    {
+                        AllBusStopList.map((BusStop, i) => {
+                            return (
+                                <Tab label={BusStop.to} key={i} value={i} ></Tab>
+                            )
+                        })
+                    }
+                </Tabs>
+            </Box>
             <div className="flex bg-white overflow-scroll whitespace-normal">
-                {
-                    AllBusStopList.map((BusStop, i) => {
-                        return (
-                            <div key={i} className='timetable' ref={(el) => {
-                                if (el) {
-                                    timetableRefs.current[i] = el
-                                }
-                            }}>
-                                {BusStop.TimeTableData ? <ShowOneDayBusTime timeTable={BusStop.TimeTableData}></ShowOneDayBusTime> : <></>}
-                            </div>
-                        )
-                    })
-                }
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    onSlideChange={(swiper) => { handleChange(swiper.activeIndex) }}
+                    ref={swiperRef}
+                // onSwiper={(swiper) => console.log(swiper)}
+                >
+                    {
+                        AllBusStopList.map((BusStop, i) => {
+                            return (
+                                <SwiperSlide key={i} >
+                                    <div className='timetable' ref={(el) => {
+                                        if (el) {
+                                            timetableRefs.current[i] = el
+                                        }
+                                    }}>
+                                        {BusStop.TimeTableData ? <ShowOneDayBusTime timeTable={BusStop.TimeTableData}></ShowOneDayBusTime> : <></>}
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+                </Swiper>
             </div>
         </div>
     )
