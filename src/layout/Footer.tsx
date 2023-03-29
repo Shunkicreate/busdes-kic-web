d0e36fe8e30bd8ac72888371dd6bdc8e933b364c
YukaChoco <sugar.ore.yk@gmail.com>
2023-03-29 17:06:33 +0900

    時刻表のフッターを前面に変更

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -36,7 +36,7 @@ export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React
     return (
         <div>
             <DisplayAddButton></DisplayAddButton>
-            <div className='fixed bottom-0 w-full grid grid-cols-3 gap-2 pt-3 pb-6 px-10 bg-white'>
+            <div className='fixed bottom-0 w-full grid grid-cols-3 gap-2 pt-3 pb-6 px-10 bg-white z-50'>
                 <TabbarButton mode='TimeTable' modeName='時刻表' color={color('TimeTable', currentMode)} switchMode={switchMode}></TabbarButton>
                 <TabbarButton mode='NextBus' modeName='次のバス' color={color('NextBus', currentMode)} switchMode={switchMode}></TabbarButton>
                 <TabbarButton mode='Settings' modeName='設定' color={color('Settings', currentMode)} switchMode={switchMode}></TabbarButton>
