48ed56f39e732d36c3ad4d2629672624376962cd
YukaChoco <sugar.ore.yk@gmail.com>
2023-03-29 15:32:29 +0900

    フッターとAddButton固定

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -20,12 +20,20 @@ export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React
 
     return (
         <div>
-            <div className="flex justify-center">
-                <TabbarButton mode="TimeTable" modeName='時刻表' color={color('TimeTable', currentMode)} switchMode={switchMode}></TabbarButton>
-                <TabbarButton mode="NextBus" modeName='次のバス' color={color('NextBus', currentMode)} switchMode={switchMode}></TabbarButton>
-                <TabbarButton mode="Settings" modeName='設定' color={color('Settings', currentMode)} switchMode={switchMode}></TabbarButton>
-                <AddButton></AddButton>
+            <DisplayAddButton></DisplayAddButton>
+            <div className='fixed bottom-0 w-full grid grid-cols-3 gap-2 pt-3 pb-6 px-10 bg-white'>
+                <TabbarButton mode='TimeTable' modeName='時刻表' color={color('TimeTable', currentMode)} switchMode={switchMode}></TabbarButton>
+                <TabbarButton mode='NextBus' modeName='次のバス' color={color('NextBus', currentMode)} switchMode={switchMode}></TabbarButton>
+                <TabbarButton mode='Settings' modeName='設定' color={color('Settings', currentMode)} switchMode={switchMode}></TabbarButton>
             </div>
+        </div>
+    )
+}
+
+const DisplayAddButton = () => {
+    return (
+        <div className='fixed bottom-24 right-0 p-5'>
+            <AddButton></AddButton>
         </div>
     )
 }
