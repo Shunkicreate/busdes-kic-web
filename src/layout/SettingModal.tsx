b1329bd014133d15e62726254e49578b3f527fc6
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-02 01:47:27 +0900

    デザインはうまくいくようにした

--- src/layout/SettingModal.tsx
+++ src/layout/SettingModal.tsx
@@ -1,32 +1,19 @@
 import React from 'react';
 import { useModal } from 'react-hooks-use-modal';
 import { useState } from 'react';
-import { AllBusStopsType, AllBusStops, busRouteAtomType, busStopListAtomType } from '../types/Bus.type';
-import { useRecoilValue, useSetRecoilState } from 'recoil';
-import busRouteAtom from '../grobalState/atoms/busRoute';
-import swapBusRouteSelector from '../grobalState/selectors/swapBusRoute';
-import setpBusRouteSelector from '../grobalState/selectors/setBusRoute';
+import { AllBusStopsType, AllBusStops, busStopListAtomType } from '../types/Bus.type';
+import { useSetRecoilState } from 'recoil';
 import addAllBusStopListSelector from '../grobalState/selectors/addAllBusStopList';
 import { ApproachInfos } from '../types/Bus.type';
 import Addbutton from '../images/addButton.svg'
 
 const SettingModal = () => {
-    const [Modal, open, close, isOpen] = useModal('root', {
+    const [Modal, open, close] = useModal('root', {
         preventScroll: true
     })
-    const modalStyle: React.CSSProperties = {
-        backgroundColor: '#fff',
-        padding: '60px 100px',
-        borderRadius: '10px',
-    };
-    const busRoute = useRecoilValue<busRouteAtomType>(busRouteAtom)
-    const swapBusRoute = useSetRecoilState(swapBusRouteSelector)
-    const setBusRoute = useSetRecoilState(setpBusRouteSelector)
-    const [select, setSelect] = useState('')
+    const [select, setSelect] = useState<AllBusStopsType | ''>('')
     const addAllBusStopList = useSetRecoilState(addAllBusStopListSelector)
-
     const TestData: ApproachInfos = {
-
         'approach_infos': [
             {
                 more_min: undefined,
@@ -41,23 +28,8 @@ const SettingModal = () => {
         ]
     }
 
-
-    const upDateStation = (value: string) => {
+    const upDateStation = (value: AllBusStopsType) => {
         setSelect(value)
-        if (busRoute.fr === '立命館大学前') {
-            const newRoute: busRouteAtomType = {
-                fr: '立命館大学前',
-                to: value as AllBusStopsType
-            }
-            setBusRoute(newRoute)
-        }
-        else if (busRoute.to === '立命館大学') {
-            const newRoute: busRouteAtomType = {
-                fr: value as AllBusStopsType,
-                to: '立命館大学'
-            }
-            setBusRoute(newRoute)
-        }
     }
 
     const onClickEventHandle = () => {
@@ -65,47 +37,89 @@ const SettingModal = () => {
     }
 
     const addSettingList = () => {
-        const addBusStop: busStopListAtomType[] = [{
-            fr: busRoute.fr,
-            to: busRoute.to,
-            ShowTimeTable: true,
-            ShowBusCard: true,
-            TimeTableData: undefined,
-            BusCardData: TestData,
-        }]
-        addAllBusStopList(addBusStop)
-        close()
+        if (!select) {
+            alert('バス停を選択してください')
+        }
+        else if (!(AllBusStops.includes(select))) {
+            alert('正式なバス停の名前を選択してください。')
+        }
+        else {
+            let addBusStop: busStopListAtomType[] = [{
+                fr: select,
+                to: '立命館大学',
+                ShowTimeTable: true,
+                ShowBusCard: true,
+                TimeTableData: undefined,
+                BusCardData: TestData,
+            }]
+            addAllBusStopList(addBusStop)
+            addBusStop = [{
+                fr: '立命館大学前',
+                to: select,
+                ShowTimeTable: true,
+                ShowBusCard: true,
+                TimeTableData: undefined,
+                BusCardData: TestData,
+            }]
+            addAllBusStopList(addBusStop)
+            close()
+        }
+    }
+
+    const isNotRits = (BusStop: AllBusStopsType) => {
+        if (BusStop === '立命館大学前' || BusStop === '立命館大学') {
+            return false
+        }
+        else {
+            return true
+        }
     }
 
     return (
         <div>
-            {/* <div>Modal is Open? {isOpen ? 'Yes' : 'No'}</div> */}
             <button onClick={open}>
                 <img className="" src={Addbutton} alt="AddButton" width="50" />
             </button>
             <Modal>
-                <button onClick={close} className='text-white'>CLOSE</button>
-                <div style={modalStyle}>
-                    <div onClick={() => { swapBusRoute(busRoute) }}>
-                        入れ替え
+                <div className="bg-main rounded-2xl w-[calc(100vw-4rem)] m-4">
+                    <div className='text-center font-bold py-4 relative'>
+                        <button onClick={close} className='absolute w-fit h-fit top-0 bottom-0 left-6 m-auto'>
+                            戻る
+                        </button>
+                        <div>
+                            路線の追加
+                        </div>
                     </div>
-                    <div>
-                        <div>出発: {busRoute.fr}</div>
-                    </div>
-                    <div>
-                        <div>到着: {busRoute.to}</div>
-                        <label htmlFor='bus-stop-choice'>Choose a Bus Stop:</label>
-                        <input type='text' list='bus-stop-list' id='bus-stop-choice' name='bus-stop-choice' value={select} onChange={(event) => upDateStation(event.target.value)} placeholder='駅名を入力' onClick={() => { onClickEventHandle() }}></input>
-                        <div onClick={addSettingList}>
+                    <div className='bg-white h-[calc(100vh-12rem)] rounded-b-2xl'>
+                        <div className='text-center p-4 text-lg'>
+                            どこからバスに乗りますか？
+                        </div>
+                        <div className=''>
+                            <div className='w-fit m-auto'>
+                                <input className='bg-border rounded-full py-2 px-8' type='text' list='bus-stop-list' id='bus-stop-choice' name='bus-stop-choice' value={select as AllBusStopsType} onChange={(event) => upDateStation(event.target.value as AllBusStopsType)} placeholder='駅名を入力' onClick={() => { onClickEventHandle() }} onKeyDown={addSettingList}></input>
+                            </div>
+                            <datalist id='bus-stop-list'>
+                                {AllBusStops.filter(isNotRits).map((value, idx) => {
+                                    return (
+                                        <option value={value} key={idx}></option>
+                                    )
+                                })}
+                            </datalist>
+                        </div>
+                        <div className='m-4 overflow-scroll h-[calc(100vh-24rem)]'>
+                            {
+                                AllBusStops.filter(isNotRits).map((BusStop, i) => {
+                                    return (
+                                        <div key={i} className='m-4 text-sm' onClick={() => { setSelect(BusStop) }}>
+                                            {BusStop}
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                        <div className='text-center m-8' onClick={addSettingList}>
                             追加
                         </div>
-                        <datalist id='bus-stop-list'>
-                            {AllBusStops.map((value, idx) => {
-                                return (
-                                    <option value={value} key={idx}></option>
-                                )
-                            })}
-                        </datalist>
                     </div>
                 </div>
             </Modal>
