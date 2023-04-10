5aa520badc4a6682a10f25d95214ae54183ad731
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-11 01:04:04 +0900

    ダークモードもう一度消す

--- src/layout/BusCard.tsx
+++ src/layout/BusCard.tsx
@@ -19,7 +19,7 @@ const BusCard = (prop : Props) => {
   return (
 
 
-    <div className='w-11/12 p-2 pt-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-gray-800 dark:border-gray-700'>
+    <div className='w-11/12 p-2 pt-4 bg-white border rounded-lg shadow-md sm:p-8 dark:bg-white'>
       <BusCardHeader from_bus={prop.from} to_bus={prop.to} />
       <hr />
       <NextBusInfoFromAPI from_bus={prop.from} to_bus={prop.to} index={prop.index}/>
