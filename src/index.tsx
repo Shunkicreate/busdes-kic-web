1bbdb55b2ce1f5662fa0dca5f1bd198dfa4ac2e2
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-12 00:38:51 +0900

    アイコンの大きさ調整

--- src/index.tsx
+++ src/index.tsx
@@ -21,7 +21,7 @@ root.render(
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffe600" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg"/>
         <meta name="msapplication-TileColor" content="#ffe600" />
         <meta name="theme-color" content="#ffe600" />
       </Helmet>
