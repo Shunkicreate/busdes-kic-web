ab7e3ce037eff1ae5048d74d6b04e10e0bed9893
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-11 17:11:58 +0900

    register実行

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Helmet, HelmetProvider } from 'react-helmet-async'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -15,11 +16,17 @@ root.render(
         <title>京都バスです！</title>
         <meta name="theme-color" content="#fff"></meta>
         <meta name="apple-mobile-web-app-status-bar-style" content="white"></meta>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Helmet>
       <App />
     </HelmetProvider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
