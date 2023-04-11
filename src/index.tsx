b57b4e6e29a29c37b0ed36bf164aa8803ab59649
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-11 23:43:36 +0900

    favicon周りせってい

--- src/index.tsx
+++ src/index.tsx
@@ -17,6 +17,13 @@ root.render(
         <meta name="theme-color" content="#fff"></meta>
         <meta name="apple-mobile-web-app-status-bar-style" content="white"></meta>
         <meta name="apple-mobile-web-app-capable" content="yes" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffe600" />
+        <meta name="msapplication-TileColor" content="#ffe600" />
+        <meta name="theme-color" content="#ffe600" />
       </Helmet>
       <App />
     </HelmetProvider>
