3b4d315323e523964078b6a5a035c0a1ae457d06
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-13 17:42:00 +0900

    TimeTableのデザイン崩れたのでPadding修正

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 export const Header = () => {
     return (
-        <div className="sticky top-0 w-full text-2xl text-contentText bg-main py-3 font-bold z-50">
+        <div className="sticky top-0 w-full text-2xl text-contentText bg-main py-4 font-bold z-50">
             Busdes!
         </div>
     )
