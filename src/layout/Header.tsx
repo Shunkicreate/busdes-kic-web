c2806a4444c335bb1e7d870bb1c3451c850a8ffe
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-15 15:41:38 +0900

    reload

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import refresh from '../images/refresh.svg'
+
 export const Header = () => {
     return (
         <div className="sticky top-0 w-full text-2xl text-contentText bg-main py-4 font-bold z-10">
-            Busdes!
+            <div>
+                Busdes!
+            </div>
+            <div className='fixed w-fit h-fit right-8 top-4'>
+                <button onClick={()=>{location.reload()}}>
+                    <img src={refresh} alt="refresh button" className='w-8 h-8' />
+                </button>
+            </div>
         </div>
     )
 }
