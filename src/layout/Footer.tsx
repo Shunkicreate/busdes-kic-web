f661e3e0aa6eb932caf651ebe42b6a0a07e22d01
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-15 18:37:37 +0900

    タブバーのボタンにアイコン追加

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -2,26 +2,21 @@ import { mode } from '../types/main.type'
 import { TabbarButton } from '../atom/TabbarButton'
 import React from 'react'
 import DisplayAddButton from '../atom/DisplayAddButton'
+// import timer from '../images/timer.svg'
+// import timetable from '../images/timetable.svg'
 
 export const Footer = ({ setMode, currentMode }: { setMode: React.Dispatch<React.SetStateAction<mode>>, currentMode: mode }) => {
     const switchMode = (mode: mode) => {
         setMode(mode)
     }
-    const color = (mode: mode, currentMode: mode) => {
-        if (mode === currentMode) {
-            return 'bg-main text-white'
-        }
-        else {
-            return 'bg-bgColor'
-        }
-    }
-
     return (
         <div className='w-full fixed bottom-0 z-10'>
             <div className='relative'>
-                <div className='grid grid-cols-2 gap-4 pt-4 pb-4 px-8 bg-white border-t'>
-                    <TabbarButton mode='NextBus' modeName='NextBus' color={color('NextBus', currentMode)} switchMode={switchMode}></TabbarButton>
-                    <TabbarButton mode='TimeTable' modeName='Timetable' color={color('TimeTable', currentMode)} switchMode={switchMode}></TabbarButton>
+                <div className='grid grid-cols-2 gap-2 pt-4 pb-4 px-6 bg-white border-t'>
+                    <TabbarButton mode='NextBus' currentMode={currentMode} switchMode={switchMode}></TabbarButton>
+                    <TabbarButton mode='TimeTable' currentMode={currentMode} switchMode={switchMode}></TabbarButton>
+                    {/* <TabbarButton mode='NextBus' modeName='NextBus' img={timer} color={color('NextBus', currentMode)} switchMode={switchMode}></TabbarButton>
+                    <TabbarButton mode='TimeTable' modeName='Timetable' img={timetable} color={color('TimeTable', currentMode)} switchMode={switchMode}></TabbarButton> */}
                 </div>
                 <DisplayAddButton currentMode={currentMode}></DisplayAddButton>
             </div>
