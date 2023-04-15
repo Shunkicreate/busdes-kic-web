04638e606730b44de905646b8068cb0515c2725c
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-15 19:02:00 +0900

    駅名を中央に配置

--- src/layout/BusCardHeader.tsx
+++ src/layout/BusCardHeader.tsx
@@ -11,11 +11,11 @@ const BusCardHeader = (props: Props) => {
 
     return (
         <div className='grid grid-cols-7 gap-2 h-16 place-content-center text-center'>
-            <div className='text-xl col-span-3'>{props.from_bus}</div>
+            <div className='text-xl col-span-3 my-auto'>{props.from_bus}</div>
             <div className='flex justify-center col-span-1'>
                 <img className='' src={BusArrow} alt='BusArrow' width='45' />
             </div>
-            <div className='text-xl col-span-3'>{props.to_bus}</div>
+            <div className='text-xl col-span-3 my-auto'>{props.to_bus}</div>
         </div>
     )
 }
