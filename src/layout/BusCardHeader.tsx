b09c2afa30773b1a7d437ecf37c7e36d73cadb68
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-17 02:10:26 +0900

    押したら伸びて表示

--- src/layout/BusCardHeader.tsx
+++ src/layout/BusCardHeader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BusArrow from '../images/BusArrow.svg';
 import { AllBusStopsType } from '../types/Bus.type';
 
@@ -9,13 +9,19 @@ type Props = {
 
 const BusCardHeader = (props: Props) => {
 
+    const [showAllBusStops,setShowAllBusStops] = useState(false);
+
+    const switchShow = () => {
+        setShowAllBusStops(showAllBusStops===false?true:false);
+    }
+
     return (
-        <div className='grid grid-cols-7 gap-2 min-h-[4rem] h-fit place-content-center text-center'>
-            <div className='text-xl col-span-3 my-auto line-clamp-2'>{props.from_bus}</div>
-            <div className='flex justify-center col-span-1'>
+        <div onClick={switchShow} className='grid grid-cols-7 gap-2 min-h-[4rem] h-fit place-content-center text-center'>
+            <div className={'text-xl col-span-3 my-auto '+(showAllBusStops===false?'line-clamp-2':'')}>{props.from_bus}</div>
+            <div className='flex justify-center col-span-1 my-auto'>
                 <img className='' src={BusArrow} alt='BusArrow' width='45' />
             </div>
-            <div className='text-xl col-span-3 my-auto line-clamp-2'>{props.to_bus}</div>
+            <div className={'text-xl col-span-3 my-auto '+(showAllBusStops===false?'line-clamp-2':'')}>{props.to_bus}</div>
         </div>
     )
 }
