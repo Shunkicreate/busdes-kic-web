8fa71d5330330db136cd83ac340749ba111bdc1b
YukaChoco <sugar.ore.yk@gmail.com>
2023-04-17 23:13:04 +0900

    アドバイスの関数使う方法に変更

--- src/layout/BusCardHeader.tsx
+++ src/layout/BusCardHeader.tsx
@@ -10,18 +10,25 @@ type Props = {
 const BusCardHeader = (props: Props) => {
 
     const [showAllBusStops, setShowAllBusStops] = useState(false);
+    const [showAllBusStopsParams, setShowAllBusStopsParams] = useState<'line-clamp-2'|''>('line-clamp-2');
 
     const switchShow = () => {
-        setShowAllBusStops(showAllBusStops === false ? true : false);
+        setShowAllBusStops(!showAllBusStops)
+        if(showAllBusStops){
+            setShowAllBusStopsParams('line-clamp-2')
+        }
+        else{
+            setShowAllBusStopsParams('')
+        }
     }
 
     return (
         <div onClick={switchShow} className='grid grid-cols-7 gap-2 min-h-[4rem] h-fit place-content-center text-center'>
-            <div className={'text-xl col-span-3 my-auto ' + (showAllBusStops === false ? 'line-clamp-2' : '')}>{props.from_bus}</div>
+            <div className={'text-xl col-span-3 my-auto ' + showAllBusStopsParams}>{props.from_bus}</div>
             <div className='flex justify-center col-span-1 my-auto'>
                 <img className='' src={BusArrow} alt='BusArrow' width='45' />
             </div>
-            <div className={'text-xl col-span-3 my-auto ' + (showAllBusStops === false ? 'line-clamp-2' : '')}>{props.to_bus}</div>
+            <div className={'text-xl col-span-3 my-auto ' + showAllBusStopsParams}>{props.to_bus}</div>
         </div>
     )
 }
