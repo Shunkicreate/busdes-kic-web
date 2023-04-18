581a8d4521e6552110c7fa486a4656af97eb456f
Shunkicreate <jmsrsyunrinsyunki@gmail.com>
2023-04-18 11:29:01 +0900

    card無かったときのデザイン

--- src/layout/BuscardRoundTrip.tsx
+++ src/layout/BuscardRoundTrip.tsx
@@ -5,9 +5,33 @@ import addAllBusStopListSelector from '../grobalState/selectors/addAllBusStopLis
 import getAllBusStopList from '../grobalState/selectors/getAllBusStopList';
 import { useRecoilValue, useSetRecoilState } from 'recoil';
 
+const EmptyBusStop = () => {
+    return (
+        <div className='fixed top-0 left-0 right-0 bottom-0 bg-bgColor'>
+            <div className='absolute w-fit h-fit top-[50%] right-0 left-0 m-auto'>
+                <div>
+                    バス停が登録されていません
+                </div>
+                <div>
+                    右下の追加ボタンからバス停を追加してください
+                </div>
+            </div>
+        </div>
+    )
+}
 const RoundTripCard = () => {
 
     const AllBusStopList = useRecoilValue(getAllBusStopList)
+
+    let emptyFlag = true
+    AllBusStopList.filter((BusStop) => BusStop.ShowTimeTable).forEach(() => {
+        emptyFlag = false
+    })
+    if (emptyFlag || AllBusStopList.length === 0) {
+        return (
+            <EmptyBusStop></EmptyBusStop>
+        )
+    }
 
     const BusStops = AllBusStopList.map((busstop, i) => {
 
